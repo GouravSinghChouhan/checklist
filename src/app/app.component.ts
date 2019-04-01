571e2a51f54244bd3caa13503c64d033510dff20
GouravSinghChouhan <gouravthakur0000@gmail.com>
2019-04-02 01:59:59 +0530

    three rest api done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'learnangularmaterial';
+export class AppComponent implements OnInit {
+
+  newListName = '';
+  isdelete = false;
+  listArray = [
+  ];
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:4500/api/checklist').subscribe((response: any) => {
+      this.listArray = response;
+    });
+  }
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  createNewList(event: any) {
+    this.newListName = (<HTMLInputElement>event.target).value;
+  }
+
+  onCreateList() {
+    if (this.newListName != '') {
+      let listObj = {};
+      listObj["id"] = this.listArray.length;
+      listObj["name"] = this.newListName;;
+      this.listArray.push(listObj);
+      this.http.post('http://localhost:4500/api/checklist', {
+        name: this.newListName
+      }).subscribe((response) => {
+        console.log(response);
+
+      });
+
+      this.newListName = '';
+    }
+  }
+
+  deleteList(listId: any, list:any) {
+    this.listArray.splice(this.listArray.indexOf(list),1);
+    console.log(this.listArray);
+    console.log(listId+"...");
+    // const params = new HttpParams().set('id', listId);
+    this.http.delete('http://localhost:4500/api/checklist/'+listId).subscribe(response => {
+      console.log(response);
+      err => console.error(err)
+    });
+    // this.ngOnInit();
+  }
+
+  enableDelete() {
+    this.isdelete = true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +15,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
